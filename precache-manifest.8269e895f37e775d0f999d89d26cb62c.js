34637e7ffd94820afcb9e35038e6e1821e6ad6ba
Wudream <1941190269@qq.com>
2022-10-17 17:01:10 +0800

    Updates

---
+++ precache-manifest.8269e895f37e775d0f999d89d26cb62c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b20f33d54525cdb85ef690bc04666bd6",
+    "url": "/memories/index.html"
+  },
+  {
+    "revision": "e4edacba814c5e503828",
+    "url": "/memories/static/css/main.8874ce43.chunk.css"
+  },
+  {
+    "revision": "2ceb97263621590ea320",
+    "url": "/memories/static/js/2.eaf08253.chunk.js"
+  },
+  {
+    "revision": "4b2087d7283ac9223e6e3252595f9784",
+    "url": "/memories/static/js/2.eaf08253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4edacba814c5e503828",
+    "url": "/memories/static/js/main.a65f2f82.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/memories/static/js/main.a65f2f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0527c78885c833e93cf0",
+    "url": "/memories/static/js/runtime-main.87933d23.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/memories/static/media/memories.5c0c63fb.png"
+  }
+]);
